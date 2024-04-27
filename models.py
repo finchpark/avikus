7edd630458b78d8eae0a21daf442da15b018cb92
Sooneui Kwon <finchpark@naver.com>
2024-04-27 13:35:20 +0900

    add model

--- models.py
+++ models.py
@@ -2,9 +2,12 @@ from sqlalchemy import Column, Integer, String, Text, DataTime
 
 from database import Base
 
+# model 생성
 class Vessel(Base):
+	# 모델이 사용할 Table
 	__tablename__ = "vessel"
 
+	# Model의 Column 속성
 	id = Column(Integer, primary_key=True)
 	created = Column(DataTime, nulable=False)
 	name = Column(String, nullable=False)
